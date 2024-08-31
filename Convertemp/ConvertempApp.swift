9b16435baef577dcfb4a0b763054840b43abd3d6
JoshuaRosado <joshua06rosado@gmail.com>
2024-08-31 15:28:06 -0500

    Initial Commit

---
+++ Convertemp/ConvertempApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConvertempApp.swift
+//  Convertemp
+//
+//  Created by Joshua Rosado Olivencia on 8/31/24.
+//
+
+import SwiftUI
+
+@main
+struct ConvertempApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
